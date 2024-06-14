2751ec369662e4d5eacb5f0c2d73b19c3d366a0c
Hadi Faraz <hadifaraz52@protonmail.com>
2024-06-13 23:49:12 -0600

    Add `mason-lspconfig` configuration setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,11 +2,12 @@ return {
     {
 	"williamboman/mason.nvim",
         lazy = false,
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim"
+        },
         init = function()
             require("mason").setup()
+            require("mason-lspconfig").setup()
         end,
     },
-    {
-	"williamboman/mason-lspconfig.nvim",
-    }
 }
